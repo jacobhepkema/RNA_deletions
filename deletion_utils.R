f26c2454bb93d15ff4efa69d428580175766596f
Jacob Hepkema <41913625+jacobhepkema@users.noreply.github.com>
2021-07-07 11:34:27 +0100

    Update deletion_utils.R

--- deletion_utils.R
+++ deletion_utils.R
@@ -94,3 +94,186 @@ get_spans <- function(bam_gr_df, templ){
   }
   return(spans)
 }
+
+
+# Get deletion information without using overlaps with exons/regions
+get_deletion_df_no_overlaps <- function(bam_gr_df,
+                                        templ,
+                                        min_length = 14){
+  require(stringr)
+  deletion_names <- c()
+  deletion_starts <- c()
+  deletion_ends <- c()
+  deletion_strands <- c()
+  deletion_sizes <- c()
+  deletion_idx <- c()
+  deletion_span <- c()
+  pb <- txtProgressBar(0, nrow(bam_gr_df), style=3)
+  for(i in 1:nrow(bam_gr_df)){
+    setTxtProgressBar(pb, i)
+    cigar_numbers <- as.numeric(strsplit(gsub(bam_gr_df$cigar[i], pattern = "\\.", replacement = ""), "[A-Z]+")[[1]])
+    cigar_letters <- strsplit(gsub(bam_gr_df$cigar[i], pattern = "\\.", replacement = ""), "[0-9]+")[[1]][-c(1)]
+    # remove insertions
+    cigar_numbers <- cigar_numbers[cigar_letters != "I"]
+    cigar_letters <- cigar_letters[cigar_letters != "I"]
+    # remove starting/trailing nucleotides
+    cigar_numbers <- cigar_numbers[cigar_letters != "S"]
+    cigar_letters <- cigar_letters[cigar_letters != "S"]
+    # remove hard clipping
+    cigar_numbers <- cigar_numbers[cigar_letters != "H"]
+    cigar_letters <- cigar_letters[cigar_letters != "H"]
+    consider <- 1:(length(cigar_numbers))
+    # check if read has deletion of at least length X in exon
+    if(sum(cigar_numbers[consider][cigar_letters[consider] %in% c("D", "N")] > min_length) != 0){
+      # cycle through deletions
+      for(d in 1:sum(cigar_letters[consider] %in% c("D", "N"))){
+        if((d != 1) & (d != length(cigar_numbers))){
+          # check if current deletion has at least length X
+          if(cigar_numbers[consider][cigar_letters[consider] %in% c("D", "N")][d] > min_length){
+            # get current deletion index
+            curr_del_idx <- consider[cigar_letters[consider] %in% c("D", "N")][d]
+            # check if CIGAR string before and after deletion cover exonic parts ("M")
+            if("M" %in% cigar_letters[consider[1]:(curr_del_idx-1)] &
+               "M" %in% cigar_letters[(curr_del_idx+1):consider[length(consider)]]){
+              # In case of intron, should not be last within exon
+              if((cigar_letters[curr_del_idx] == "N" & curr_del_idx == consider[length(consider)]) | 
+                 (cigar_letters[curr_del_idx] == "N" & curr_del_idx == consider[1])){
+                next
+              } else {
+                deletion_names <- c(deletion_names, as.character(bam_gr_df$chrom[i]))
+                deletion_start <- bam_gr_df$start[i] + sum(cigar_numbers[1:(max(1, curr_del_idx-1))])
+                deletion_end <- bam_gr_df$start[i] + sum(cigar_numbers[1:(max(1, curr_del_idx))]) - 1
+                deletion_starts <- c(deletion_starts, deletion_start)
+                deletion_ends <- c(deletion_ends, deletion_end)
+                deletion_sizes <- c(deletion_sizes, cigar_numbers[consider][cigar_letters[consider] %in% c("D", "N")][d])
+                deletion_strands <- c(deletion_strands, as.character(bam_gr_df$strand[i]))
+                deletion_idx <- c(deletion_idx, i)
+                deletion_span <- c(deletion_span, sum(cigar_numbers) / width(templ))
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  if(length(deletion_idx) > 0){
+    deletion_gr_df <- data.frame(deletion_idx=deletion_idx,
+                                 chrom=deletion_names, 
+                                 start=deletion_starts, 
+                                 end=deletion_ends, 
+                                 strand=deletion_strands, 
+                                 size=deletion_sizes,
+                                 span=deletion_span)
+    deletion_gr_df$region <- paste0(deletion_gr_df$chrom, ":", deletion_gr_df$start, "-", deletion_gr_df$end)
+    deletion_gr_df$rname <- bam_gr_df$qname[deletion_gr_df$deletion_idx]
+    deletion_gr_df$times_found <- as.numeric(table(deletion_gr_df$region)[deletion_gr_df$region])
+    return(deletion_gr_df)
+  } else {
+    return(-1)
+  }
+}
+
+
+# Get deletion information using overlaps with exons/regions
+get_deletion_df <- function(bam_gr_df, bam_overlaps, exons_gr,
+                            min_length = 14){
+  require(stringr)
+  exons_starts <- start(exons_gr)
+  exons_ends <- end(exons_gr)
+  deletion_names <- c()
+  deletion_starts <- c()
+  deletion_ends <- c()
+  deletion_strands <- c()
+  deletion_sizes <- c()
+  deletion_genes <- c()
+  deletion_exon_nrs <- c()
+  deletion_query_idx <- c()
+  deletion_subject_idx <- c()
+  pb <- txtProgressBar(0, length(bam_overlaps), style=3)
+  for(i in 1:length(bam_overlaps)){
+    setTxtProgressBar(pb, i)
+    curr_query_idx <- queryHits(bam_overlaps)[i]
+    curr_subject_idx <- subjectHits(bam_overlaps)[i]
+    # check if CIGAR string has changes in exonic region
+    cigar_numbers <- as.numeric(strsplit(gsub(bam_gr_df$cigar[curr_query_idx], pattern = "\\.", replacement = ""), "[A-Z]+")[[1]])
+    cigar_letters <- strsplit(gsub(bam_gr_df$cigar[curr_query_idx], pattern = "\\.", replacement = ""), "[0-9]+")[[1]][-c(1)]
+    # remove insertions
+    cigar_numbers <- cigar_numbers[cigar_letters != "I"]
+    cigar_letters <- cigar_letters[cigar_letters != "I"]
+    # remove starting/trailing nucleotides
+    cigar_numbers <- cigar_numbers[cigar_letters != "S"]
+    cigar_letters <- cigar_letters[cigar_letters != "S"]
+    # figure out places within read ranges relative to read start where exons begin and end
+    exon_starts_at_pos <- exons_starts[curr_subject_idx] - bam_gr_df$start[curr_query_idx]
+    add <- 0
+    for(j in 1:length(cigar_numbers)){
+      cigar_num <- cigar_numbers[j]
+      if(add + cigar_num > exon_starts_at_pos){
+        break
+      } else {
+        add <- add + cigar_num
+      }
+    }
+    exon_ends_at_pos <- exons_ends[curr_subject_idx] - bam_gr_df$start[curr_query_idx]
+    add <- 0
+    for(q in 1:length(cigar_numbers)){
+      cigar_num <- cigar_numbers[q]
+      if(add + cigar_num > exon_ends_at_pos){
+        break
+      } else {
+        add <- add + cigar_num
+      }
+    }
+    if(q < j){
+      next
+    } else {
+      consider <- j:q
+      # check if read has deletion of at least length X in exon
+      if(sum(cigar_numbers[consider][cigar_letters[consider] %in% c("D", "N")] > min_length) != 0){
+        # cycle through deletions
+        for(d in 1:sum(cigar_letters[consider] %in% c("D", "N"))){
+          # check if current deletion has at least length X
+          if(cigar_numbers[consider][cigar_letters[consider] %in% c("D", "N")][d] > min_length){
+            # get current deletion index
+            curr_del_idx <- consider[cigar_letters[consider] %in% c("D", "N")][d]
+            # check if deletion is not first or last of CIGAR string
+            if(curr_del_idx != 1 & 
+               curr_del_idx != length(cigar_numbers)){
+              # check if CIGAR string before and after deletion cover exonic parts ("M")
+              if("M" %in% cigar_letters[consider[1]:(curr_del_idx-1)] &
+                 "M" %in% cigar_letters[(curr_del_idx+1):consider[length(consider)]]){
+                # In case of intron, should not be last within exon
+                if((cigar_letters[curr_del_idx] == "N" & curr_del_idx == consider[length(consider)]) | 
+                   (cigar_letters[curr_del_idx] == "N" & curr_del_idx == consider[1])){
+                  next
+                }
+                deletion_names <- c(deletion_names, as.character(bam_gr_df$chrom[curr_query_idx]))
+                deletion_start <- bam_gr_df$start[curr_query_idx] + sum(cigar_numbers[1:(max(1, curr_del_idx-1))])
+                deletion_end <- bam_gr_df$start[curr_query_idx] + sum(cigar_numbers[1:(max(1, curr_del_idx))]) - 1
+                deletion_starts <- c(deletion_starts, deletion_start)
+                deletion_ends <- c(deletion_ends, deletion_end)
+                deletion_sizes <- c(deletion_sizes, cigar_numbers[consider][cigar_letters[consider] %in% c("D", "N")][d])
+                deletion_strands <- c(deletion_strands, as.character(bam_gr_df$strand[curr_query_idx]))
+                deletion_genes <- c(deletion_genes, exons_gr$symbol[curr_subject_idx])
+                deletion_exon_nrs <- c(deletion_exon_nrs, exons_gr$exonnumber[curr_subject_idx])
+                deletion_query_idx <- c(deletion_query_idx, curr_query_idx)
+                deletion_subject_idx <- c(deletion_subject_idx, curr_subject_idx)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  deletion_gr_df <- data.frame(query_idx=deletion_query_idx,
+                               subject_idx=deletion_subject_idx,
+                               chrom=deletion_names, 
+                               start=deletion_starts, 
+                               end=deletion_ends, 
+                               strand=deletion_strands, 
+                               symbol=deletion_genes,
+                               exon_nr=deletion_exon_nrs,
+                               size=deletion_sizes)
+  deletion_gr_df$region <- paste0(deletion_gr_df$chrom, ":", deletion_gr_df$start, "-", deletion_gr_df$end)
+  return(deletion_gr_df)
+}
